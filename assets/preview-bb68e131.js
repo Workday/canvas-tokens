0ed260d826015396e17b8daeea8df7d6b0086ee7
alanbsmith <a.bax.smith@gmail.com>
2023-10-25 22:32:17 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@42b69d398e51910222fef1b11c65e124414a7651 🚀

--- assets/preview-bb68e131.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-8bb1fb02.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-f20edd18.js"),["./DocsRenderer-3PUGWF3O-f20edd18.js","./iframe-8bb1fb02.js","./index-ebeaab24.js","./react-18-8ac2d3a8.js","./index-073301bc.js","./index-6cc5148b.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-bb68e131.js.map
