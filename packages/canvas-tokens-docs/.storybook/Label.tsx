0feede1187ccbddebfc04738e833ea24ee1d1667
Manuel Carrera <mannycarrera4@users.noreply.github.com>
2025-07-21 13:08:18 -0600

    Update Label.tsx

--- packages/canvas-tokens-docs/.storybook/Label.tsx
+++ packages/canvas-tokens-docs/.storybook/Label.tsx
@@ -2,28 +2,49 @@ import React from 'react';
 
 const flexStyles = {
   display: 'flex',
-  alignItems: 'center',
+  alignItems: 'flex-start',
   gap: '8px',
   width: '100%',
   justifyContent: 'space-between',
   paddingInlineEnd: '16px',
 };
 
-const deprecatedBoxStyles = {
+const boxStyles = {
   padding: '0 8px',
-  backgroundColor: 'rgba(219, 225, 233, 1)',
-  color: 'rgba(93, 103, 117, 1)',
   borderRadius: '40px',
   fontSize: '12px',
   fontWeight: 'bold',
+  marginTop: '2px',
+  textTransform: 'capitalize',
+} as const;
+
+const getType = (name: string) => {
+  return name.match(/\((?<type>.*)\)$/)?.groups?.type;
 };
 
-export const Label = (item: {name: string}) => {
-  if (item.name.endsWith('(deprecated)')) {
+const getColors = (type: string) => {
+  switch (type) {
+    case 'AI':
+      return {
+        backgroundColor: 'rgba(207, 235, 255, 1)',
+        color: 'rgba(2, 32, 67, 1)',
+      };
+    default:
+      return {
+        backgroundColor: 'rgba(219, 225, 233, 1)',
+        color: 'rgba(93, 103, 117, 1)',
+      };
+  }
+};
+
+export const Label = item => {
+  const type = getType(item.name);
+
+  if (type) {
     return (
-      <div style={flexStyles}>
-        <span>{item.name.replace('(deprecated)', '')}</span>
-        <span style={deprecatedBoxStyles}>Deprecated</span>
+      <div className="sb-unstyled" style={flexStyles}>
+        <span>{item.name.replace(`(${type})`, '')}</span>
+        <span style={{...boxStyles, ...getColors(type)}}>{type}</span>
       </div>
     );
   }
