94cd7119ddb462be1faa0c1fa1911108120a29d9
Raisa Primerova <riyuhide@gmail.com>
2025-09-10 15:42:21 -0600

    chore: Fix docs links

--- packages/canvas-tokens-docs/components/AnnouncementBanner/index.tsx
+++ packages/canvas-tokens-docs/components/AnnouncementBanner/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 export const AnnouncementBanner = ({img, link, linkText}) => {
   return (
     <a href={link} className="announcement-banner" aria-label={linkText}>
-      <img alt={linkText} src={`../images/${img}`} width="100%" />
+      <img alt={linkText} src={img} width="100%" />
       <span className="cnvs-sys-type-subtext-large">{linkText}</span>
     </a>
   );
