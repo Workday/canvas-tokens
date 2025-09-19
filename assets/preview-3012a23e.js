29fff5590fb5b46bc8d18bbfbec112b6ca2edd13
RayRedGoose <48605821+RayRedGoose@users.noreply.github.com>
2025-09-19 22:18:56 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@3ce131ecb25c065733aa70a7276383cc827e2fd9 🚀

--- assets/preview-3012a23e.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-c8294f74.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-3739cec2.js"),["./DocsRenderer-3PUGWF3O-3739cec2.js","./iframe-c8294f74.js","./index-1b03fe98.js","./react-18-ff543a5b.js","./index-91af8003.js","./index-81261234.js","./chunk-6P7RB4HF-36fee097.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-3012a23e.js.map
