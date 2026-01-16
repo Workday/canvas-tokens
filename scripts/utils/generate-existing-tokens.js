2a6060eee496f65bca3cf013194f22b6fb6bdc2c
Raisa Primerova <riyuhide@gmail.com>
2026-01-16 09:42:45 -0700

    fix: Log path

--- scripts/utils/generate-existing-tokens.js
+++ scripts/utils/generate-existing-tokens.js
@@ -6,5 +6,7 @@ import {generateExistingTokens} from './get-tokens-array.js';
 const _root = '../../';
 const _dir = path.join(_root, 'packages/canvas-tokens-web');
 
+console.log(_dir);
+
 const tokens = generateExistingTokens();
 fs.writeFileSync(path.join(_dir, 'existing-tokens.json'), JSON.stringify(tokens, null, 2));
