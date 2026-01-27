db96e276008691bd4caca374cd56724ff349f6ca
RayRedGoose <48605821+RayRedGoose@users.noreply.github.com>
2026-01-27 15:13:17 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@5584a6345306df9b17f9830f5379f2ff845df4ae 🚀

--- assets/syntaxhighlighter-MJWPISIS-5ae442c9.js
+++
@@ -1,1 +0,0 @@
-import{S as c,c as l,s as n}from"./index-57585e15.js";import"./iframe-f1635929.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-1b03fe98.js";import"./index-6fd5a17b.js";import"./index-d7bb098e.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
