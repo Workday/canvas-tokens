bb5cb4ce8b03f730c22c604a33ba58d4c2146318
RayRedGoose <48605821+RayRedGoose@users.noreply.github.com>
2026-01-29 19:14:50 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@e54647d9e86106a599dc44c3c478fa0d88c76248 🚀

---
+++ assets/ColorShadow.stories-6cf893c6.js
@@ -0,0 +1,12 @@
+import{M as d,U as m}from"./index-28292b98.js";import{j as o,a as r,F as l}from"./jsx-runtime-86dfebf6.js";import{b as i,s as h,C as p}from"./ColorGrid-02eb65c2.js";import{c as a}from"./index-4ff1bdec.js";import{u as c}from"./index-2ef8b458.js";import"./iframe-61707770.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-1b03fe98.js";import"./index-6fd5a17b.js";import"./index-d7bb098e.js";import"./index-356e4a49.js";import"./index-bd8a2091.js";const w=i("system.color.shadow",{base:a.shadow.base,ambient:a.shadow.ambient}).sort(h),y=()=>o(p,{name:"Shadow Colors",palette:w,variableType:"system"});function n(e){const s=Object.assign({h1:"h1",p:"p",h2:"h2",pre:"pre",code:"code",hr:"hr"},c(),e.components);return r(l,{children:[o(d,{title:"Docs/System Tokens/Color/Shadow"}),`
+`,r(m,{children:[o(s.h1,{id:"shadow-colors",children:"Shadow Colors"}),o(s.p,{children:"System shadow color tokens provide values for box shadow."}),o(s.h2,{id:"usage",children:"Usage"}),o(s.pre,{children:o(s.code,{className:"language-ts",children:`// styles.ts
+import {system} from '@workday/canvas-tokens-web';
+
+const styles = {
+  boxShadow: \`0 0 0 var(\${system.color.shadow['1']})\`,
+};
+`})}),o(s.pre,{children:o(s.code,{className:"language-css",children:`// styles.css
+.card-text {
+  boxshadow: 0 0 0 var(--cnvs-sys-color-shadow-1);
+}
+`})}),o(s.hr,{}),o(s.h2,{id:"tokens",children:"Tokens"}),o(y,{}),o(s.hr,{})]})]})}function u(e={}){const{wrapper:s}=Object.assign({},c(),e.components);return s?o(s,{...e,children:o(n,{...e})}):n(e)}const g=()=>{throw new Error("Docs-only story")};g.parameters={docsOnly:!0};const t={title:"Docs/System Tokens/Color/Shadow",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:u};const P=["__page"];export{P as __namedExportsOrder,g as __page,t as default};
---
+++ assets/WithTooltip-V3YHNWJZ-490edb1e.js
@@ -0,0 +1,1 @@
+import{W as e,W as h,a as l}from"./index-28292b98.js";import"./iframe-61707770.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-1b03fe98.js";import"./index-6fd5a17b.js";import"./index-d7bb098e.js";import"./index-356e4a49.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
