e860d9e5494bef85460db0bea18f5f063d6a010e
RayRedGoose <48605821+RayRedGoose@users.noreply.github.com>
2026-02-09 19:58:30 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@4423ac38e2e62f9d66fb0cd8d94f3cc6374bb092 🚀

---
+++ assets/FontSize.stories-f9ac9a9c.js
@@ -0,0 +1,7 @@
+import{F as r}from"./FontSize-d670a264.js";import"./jsx-runtime-86dfebf6.js";import"./index-1b03fe98.js";import"./index-bd8a2091.js";import"./index-8d936b41.js";const c={component:r,title:"Visual Tests/System Tokens"},e={parameters:{chromatic:{disableSnapshot:!1}}};var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  parameters: {
+    chromatic: {
+      disableSnapshot: false
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const d=["FontSize"];export{e as FontSize,d as __namedExportsOrder,c as default};
