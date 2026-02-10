bcb06b56af1e83c27da044798e91a6d4cc89f1ab
Raisa Primerova <riyuhide@gmail.com>
2026-02-10 12:01:17 -0700

    fix: Fix docs and prop name

--- packages/canvas-tokens/utils/formatters/formatTokensInfoExport.ts
+++ packages/canvas-tokens/utils/formatters/formatTokensInfoExport.ts
@@ -1,7 +1,7 @@
 import {Dictionary, Formatter} from 'style-dictionary';
 import {json2csv} from 'json-2-csv';
 
-const getTokensInfo = (tokens: Dictionary['allProperties']) => {
+const getTokensInfo = (tokens: Dictionary['allTokens']) => {
   return tokens.map(prop => ({
     name: prop.path.join('.'),
     level: prop.path[0],
@@ -13,13 +13,22 @@ const getTokensInfo = (tokens: Dictionary['allProperties']) => {
   }));
 };
 
+/**
+ * Formatter creating the json file structure with tokens info
+ * @param {FormatterArguments} - Style Dictionary formatter object containing `dictionary` property.
+ * @returns file content as a string
+ */
 export const formatTokensInfoExport: Formatter = ({dictionary}) => {
-  const tokens = getTokensInfo(dictionary.allProperties);
+  const tokens = getTokensInfo(dictionary.allTokens);
   return JSON.stringify(tokens, null, 2);
 };
 
+/**
+ * Formatter creating the csv file structure with tokens info
+ * @param {FormatterArguments} - Style Dictionary formatter object containing `dictionary` property.
+ * @returns file content as a string
+ */
 export const formatTokensInfoExportCSV: Formatter = ({dictionary}) => {
-  const tokens = getTokensInfo(dictionary.allProperties);
-
+  const tokens = getTokensInfo(dictionary.allTokens);
   return json2csv(tokens, {emptyFieldValue: ''});
 };
