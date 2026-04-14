3c702424ffeffea11c15329527656cf80f02ef43
RayRedGoose <48605821+RayRedGoose@users.noreply.github.com>
2026-04-14 19:52:34 +0000

    Deploying to gh-pages from @ Workday/canvas-tokens@7be410bd1b1f1510a429f7c8216a665e31929850 🚀

---
+++ assets/ColorBorder.stories-WCshPc7m.js
@@ -0,0 +1,12 @@
+import{M as c,U as i}from"./index-cygeWmm5.js";import{B as d}from"./Border-OHtSm8XU.js";import{j as e,a as s,F as m}from"./jsx-runtime-CJPOobNs.js";import{useMDXComponents as a}from"./index-DoV7cT2f.js";import"./iframe-CrcroC0R.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-B8Arh_2W.js";import"./index-QF83YFrf.js";import"./index-ChsGqxH_.js";import"./index-BRq5TIsn.js";import"./index-DyTWsQK6.js";import"./index-DrFu-skq.js";import"./ColorGrid-Bs-sg3ak.js";import"./index-DdDtHz5p.js";import"./index-CMGJTjKq.js";function n(o){const r=Object.assign({h1:"h1",p:"p",h2:"h2",pre:"pre",code:"code"},a(),o.components);return s(m,{children:[e(c,{title:"Docs/System Tokens/Color/Border"}),`
+`,s(i,{children:[e(r.h1,{id:"border-colors",children:"Border Colors"}),e(r.p,{children:"System border color tokens provide values to create clear delineations between content."}),e(r.h2,{id:"usage",children:"Usage"}),e(r.pre,{children:e(r.code,{className:"language-ts",children:`// styles.ts
+import {system} from '@workday/canvas-tokens-web';
+
+const styles = {
+  borderColor: \`var(\${system.color.border.container})\`,
+};
+`})}),e(r.pre,{children:e(r.code,{className:"language-css",children:`// styles.css
+.card {
+  border-color: var(--cnvs-sys-color-border-container);
+}
+`})}),e(r.h2,{id:"tokens",children:"Tokens"}),e(d,{})]})]})}function l(o={}){const{wrapper:r}=Object.assign({},a(),o.components);return r?e(r,{...o,children:e(n,{...o})}):n(o)}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const t={title:"Docs/System Tokens/Color/Border",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:l};const B=["__page"];export{B as __namedExportsOrder,p as __page,t as default};
--- assets/WithTooltip-V3YHNWJZ-H9wjdQEX.js
+++
@@ -1,1 +0,0 @@
-import{W as h,W as l,a as s}from"./index-2WM9CWcf.js";import"./iframe-Dzh_dVr4.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-B8Arh_2W.js";import"./index-QF83YFrf.js";import"./index-ChsGqxH_.js";import"./index-BRq5TIsn.js";import"./index-DyTWsQK6.js";import"./index-DrFu-skq.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
